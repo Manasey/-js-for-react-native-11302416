a34077b0f2dd681e097dcb388a51ca06affef97a
Manasey <msbavug@st.ug.edu.gh>
2024-05-19 20:29:17 +0100

    Made changes in file

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,38 +1,31 @@
 function processArray(arr) {
-    // Create a new array to store the results
     const newArr = [];
   
-    // Loop through each number in the input array
     for (let num of arr) {
-      // Check if the number is even
       if (num % 2 === 0) {
-        // Square the even number and add it to the new array
         newArr.push(num * num);
       } else {
-        // Triple the odd number and add it to the new array
         newArr.push(num * 3);
       }
     }
   
-    // Return the new array with modified values
+
     return newArr;
   }
   
   function formatArrayStrings(strArr, numArr) {
-    // Input validation: Check if arrays have the same length
+
     if (strArr.length !== numArr.length) {
       throw new Error("Input arrays must have the same length");
     }
-  
-    // Create a new array to store formatted strings
+
     const formattedArr = [];
   
-    // Loop through strings and corresponding numbers
+
     for (let i = 0; i < strArr.length; i++) {
       const str = strArr[i];
       const num = numArr[i];
   
-      // Modify string based on number
       if (num % 2 === 0) {
         formattedArr.push(str.toUpperCase());
       } else {
@@ -40,11 +33,11 @@ function processArray(arr) {
       }
     }
   
-    // Return the new array with formatted strings
+   
     return formattedArr;
   }
   
-  // Example usage
+
   const arr = [1, 2, 3, 4, 5];
   const strArr = ["Hi", "my", "name", "is", "manasseh"];
   const result = processArray(arr);
